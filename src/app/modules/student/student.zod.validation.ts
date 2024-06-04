c8cd6f10f8451c044172119e7fba9d88d6c98f33
Zenith-Islami-Life-Insurance <zililbdit@gmail.com>
2024-06-04 10:19:55 +0600

    accept

--- src/app/modules/student/student.zod.validation.ts
+++ src/app/modules/student/student.zod.validation.ts
@@ -1,33 +1,31 @@
 import { z } from "zod";
 
-
-// Guardian schema
-const guardianSchema = z.object({
-  fatherName: z.string().min(1, { message: "Father's name is required" }),
-  motherName: z.string().min(1, { message: "Mother's name is required" }),
-  motherNumber: z.string().min(1, { message: "Mother's number is required" }),
-  fatherNumber: z.string().min(1, { message: "Father's number is required" }),
+// GUrdian interface validation schema
+const gurdianSchema = z.object({
+  fatherName: z.string().min(1, "Father's name cannot be empty"),
+  motherName: z.string().min(1, "Mother's name cannot be empty"),
+  motherNumber: z.string().min(1, "Mother's contact number cannot be empty"),
+  fatherNumber: z.string().min(1, "Father's contact number cannot be empty"),
 });
-
 
 // Student interface validation schema
 const createStudentValidation = z.object({
-  id: z.string().min(1, { message: "ID is required" }),
-  name: z.object({
-    firstName: z.string().min(1, { message: "First name is required" }),
-    lastName: z.string().min(1, { message: "Last name is required" }),
-  }).refine(data => data.firstName && data.lastName, {
-    message: "Name is required",
-    path: ["name"],
+  body: z.object({
+    password: z.string().max(20, "pass is required"),
+    student: z.object({
+      name: z.object({
+        firstName: z.string().min(1, "First name cannot be empty"),
+        lastName: z.string().min(1, "Last name cannot be empty"),
+      }),
+      dateOfBirth: z.string().min(1, "Date of birth cannot be empty"),
+      email: z.string().email("Invalid email format"),
+      gender: z.enum(["female", "male", "other"]),
+      contactNumber: z.string().optional(),
+      profileImg: z.string().optional(),
+      bloodGroup: z.enum(["A+", "B+", "O+"]),
+      gurdian: gurdianSchema,
+    }),
   }),
-  dateOfBirth: z.string().min(1, { message: "Date of birth is required" }),
-  email: z.string().email({ message: "Invalid email address" }).min(1, { message: "Email is required" }),
-  gender: z.enum(["female", "male", "other"], { required_error: "Gender is required" }),
-  contactNumber: z.string().optional(),
-  profileImg: z.string().optional(),
-  bloodGroup: z.enum(["A+", "B+", "O+"], { required_error: "Blood group is required" }),
-  gurdian: guardianSchema,
-
 });
 export const studentValidations = {
   createStudentValidation,
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -5,22 +5,22 @@ import { studentValidations } from "../student/student.zod.validation";
 
 const route = express.Router();
 
-// const validateRequest = (schema: AnyZodObject) => {
-//     return async (req: Request, res: Response, next: NextFunction) => {
-//         try {
-//             await schema.parseAsync({
-//                 body: req.body,
-//             });
-//             next();
-//         } catch (err) {
-//             next(err);
-//         }
-//     };
-// };
+const validateRequest = (schema: AnyZodObject) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await schema.parseAsync({
+                body: req.body,
+            });
+            next();
+        } catch (err) {
+            next(err);
+        }
+    };
+};
 
 route.post(
     "/create-student",
-    // validateRequest(studentValidations.createStudentValidation),
+    validateRequest(studentValidations.createStudentValidation),
     userController.createStudent
 );
 
